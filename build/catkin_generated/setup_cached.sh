b5025f1ef4dc3b889c21999c5d491cb4e800821b
1225 <2039616642@qq.com>
2025-04-19 19:46:40 +0800

    Initial commit: upload entire workspace

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,10 +8,7 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/abc/桌面/limo_ws/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/abc/桌面/limo_ws/devel/lib:$LD_LIBRARY_PATH"
 export PATH='/opt/ros/noetic/bin:/home/abc/anaconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
-export PKG_CONFIG_PATH="/home/abc/桌面/limo_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/abc/桌面/limo_ws/build'
-export PYTHONPATH="/home/abc/桌面/limo_ws/devel/lib/python3/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES='/home/abc/桌面/limo_ws/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/abc/桌面/limo_ws/src:$ROS_PACKAGE_PATH"
